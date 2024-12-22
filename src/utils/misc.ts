f87ee74c75bef59ca9a5158a820ae84f902ce958
cubos27 <cubos2769.andrade@gmail.com>
2024-12-22 09:49:27 +0000

    django rest framework change

--- src/utils/misc.ts
+++
@@ -1,7 +0,0 @@
-export function toLowerCase ( str : string ) : string {
-    return str.toLowerCase();
-}
-
-export function toTitleCase ( str : string ) : string {
-    return str.replace(/\b\w/g, l => l.toUpperCase());
-}
